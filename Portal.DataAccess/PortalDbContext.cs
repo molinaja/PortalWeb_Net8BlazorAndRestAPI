9d74229092d7b2eacd1e0f4299381c404b3cb90a
JuanMolinaVargas <jmolinav@crnova.com>
2024-10-03 19:26:24 -0600

    db context

--- Portal.DataAccess/PortalDbContext.cs
+++ Portal.DataAccess/PortalDbContext.cs
@@ -7,9 +7,28 @@ namespace Portal.DataAccess
     public class PortalDbContext : DbContext
     {
 
-        public PortalDbContext(DbContextOptions<PortalDbContext> options) : base(options)
+        public PortalDbContext(DbContextOptions<PortalDbContext> options) 
+            : base(options)
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+
+            configurationBuilder.Properties<string>()
+                .HaveMaxLength(100);
+
+            configurationBuilder.Conventions.Remove<CascadeDeleteConvention>();
+        }
+
     }
 }
---
+++ Portal.DataAccess/SecurityDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.DataAccess
+{
+    public class SecurityDbContext : IdentityDbContext<PortalIdentityUser>
+    {
+        public SecurityDbContext(DbContextOptions<SecurityDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<PortalIdentityUser>(e =>
+            {
+                e.ToTable("User");
+                //e.Property(p => p.LockoutEnd).HasColumnName("BloqueoHasta");
+                //e.Property(p => p.AccessFailedCount).HasColumnName("NroIntentos");
+            });
+
+            builder.Entity<IdentityRole>(e => e.ToTable("Role"));
+            builder.Entity<IdentityUserRole<string>>(e => e.ToTable("UserRole"));
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+
+            configurationBuilder.Properties<string>().HaveMaxLength(150);
+        }
+
+    }
+} 
